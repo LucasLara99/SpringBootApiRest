bfdebc407f495a5ba392e556dd0f9b059a528f80
Lucas Lara <lucaslara99@gmail.com>
2023-11-20 14:32:47 +0100

    CORS policy fixed

--- src/main/java/com/example/demo/controllers/ConsultaController.java
+++ src/main/java/com/example/demo/controllers/ConsultaController.java
@@ -2,14 +2,14 @@ package com.example.demo.controllers;
 
 import com.example.demo.entities.Consulta;
 import com.example.demo.services.ConsultaService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/consulta")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+
 public class ConsultaController {
     private final ConsultaService consultaService;
 
